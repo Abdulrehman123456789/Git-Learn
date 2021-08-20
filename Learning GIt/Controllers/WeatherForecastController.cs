ad1ed7b29edfeeab6d7ca74d32d23aaf2e321bc1
Abdul Rehman <ar7846020205@gmail.com>
2021-08-20 19:29:24 +0530

    'stringchange'

--- Learning GIt/Controllers/WeatherForecastController.cs
+++ Learning GIt/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Learning_GIt.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezingssss", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
