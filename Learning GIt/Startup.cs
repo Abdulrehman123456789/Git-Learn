f15df71f4defe516ddcfddfc00feeb2e4095e44c
Abdul Rehman <ar7846020205@gmail.com>
2021-08-23 08:44:29 +0530

    super

--- Learning GIt/Startup.cs
+++ Learning GIt/Startup.cs
@@ -1,6 +1,6 @@
 using System;
 using System.Collections.Genpappa;
-using System.Linq;
+using System.Linqmamma;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
