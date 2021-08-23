fc5904e5c02881996bc32b8475b6c2eca52389f3
Abdul Rehman <50143068+Abdulrehman123456789@users.noreply.github.com>
2021-08-23 09:56:55 +0530

    Update Startup.cs

--- Learning GIt/Startup.cs
+++ Learning GIt/Startup.cs
@@ -1,15 +1,4 @@
-using System;
-using System.Collections.Genpappa;
-using System.Linqmamma;
-using System.Threading.Tasksdata;
-using Microsoft.AspNetCore.Builder9632;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+
 
 namespace Learning_GIt
 {
