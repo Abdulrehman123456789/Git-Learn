807b78a66f15d2297dadf7d51942d24a388ada7f
Abdul Rehman <ar7846020205@gmail.com>
2021-08-25 14:25:53 +0530

    ss

--- Learning GIt/Program.cs
+++ Learning GIt/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Learning_GItpp
+namespace Learning_GItppss
 {
     public class Program
     {
